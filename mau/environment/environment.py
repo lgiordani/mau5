58024be4e07b0a1ef3d51752512ebcac0efe2579
Leonardo Giordani <giordani.leonardo@gmail.com>
2025-10-09 08:40:04 +0100

    Step 2

---
+++ mau/environment/environment.py
@@ -0,0 +1,78 @@
+from .helpers import flatten_nested_dict, nest_flattened_dict
+
+
+class Environment:
+    """
+    This is a class that hosts a nested configuration.
+
+    An Environment contains a dictionary of
+    configuration variables that can be queried using flat
+    variable names like `a.b.c`.
+    """
+
+    def __init__(self):
+        # This is the internal dictionary, which
+        # is always kept in its flattened version.
+        self._variables: dict = {}
+
+    @classmethod
+    def from_dict(cls, other: dict, namespace: str | None = None):
+        env = cls()
+        env.update(other, namespace)
+        return env
+
+    def update(self, other: dict, namespace: str | None = None):
+        # If there is a namespace store the
+        # new dictionary under it.
+        if namespace:
+            other = {namespace: other}
+
+        self._variables.update(flatten_nested_dict(other))
+
+    def asdict(self):
+        return nest_flattened_dict(self._variables)
+
+    def __setitem__(self, key, value):
+        self.update({key: value})
+
+    def __getitem__(self, key):
+        return self._variables[key]
+
+    def get(self, key, default=None):
+        try:
+            # If the key is present in the flat
+            # index we can just return the
+            # corresponding value.
+            return self._variables[key]
+        except KeyError:
+            # The key is not there, let's
+            # check if it works as a namespace.
+
+            # Add the dot to transform the
+            # key into a namespace prefix.
+            prefix = f"{key}."
+
+            # Find all the flat keys that
+            # start with that prefix.
+            keys = [k for k in self._variables if k.startswith(prefix)]
+
+            # If we fiund matching keys,
+            # we need to return the corresponsing
+            # items as an Environment.
+            #
+            # For each matching key, we create
+            # the key without prefix and store
+            # the value under it. Then, we
+            # promote to an Environment.
+            if len(keys) != 0:
+                return self.__class__.from_dict(
+                    {
+                        k.removeprefix(prefix): v
+                        for k, v in self._variables.items()
+                        if k.startswith(prefix)
+                    }
+                )
+
+            # If we can't find matching keys
+            # we should return the default value.
+            return default
---
+++ tests/environment/test_environment.py
@@ -0,0 +1,290 @@
+import pytest
+
+from mau.environment.environment import Environment
+
+
+def test_init():
+    environment = Environment()
+
+    assert environment.asdict() == {}
+
+
+def test_init_with_flat_content():
+    # Test that the Environment can be created
+    # with a flat dictionary.
+
+    environment = Environment.from_dict({"var1": "value1", "var2": "value2"})
+
+    assert environment.asdict() == {"var1": "value1", "var2": "value2"}
+
+
+def test_init_with_flat_content_and_namespace():
+    # Test that the Environment can be created
+    # with a flat dictionary and a given namespace.
+
+    environment = Environment.from_dict(
+        {"var1": "value1", "var2": "value2"}, namespace="parent"
+    )
+
+    assert environment.asdict() == {"parent": {"var1": "value1", "var2": "value2"}}
+
+
+def test_init_with_nested_content():
+    # Test that the Environment can be created
+    # with a nested dictionary.
+
+    environment = Environment.from_dict({"var1": {"var2": "value2"}})
+
+    assert environment.asdict() == {"var1": {"var2": "value2"}}
+
+
+def test_init_with_nested_content_and_namespace():
+    # Test that the Environment can be created
+    # with a nested dictionary and a given namespace.
+
+    environment = Environment.from_dict(
+        {"var1": {"var2": "value2"}}, namespace="parent"
+    )
+
+    assert environment.asdict() == {"parent": {"var1": {"var2": "value2"}}}
+
+
+def test_init_with_flat_hierarchical_content():
+    # Test that the Environment can be created
+    # with a flat dictionary with dotted keys.
+
+    environment = Environment.from_dict({"var1.var3": "value3", "var2": "value2"})
+
+    assert environment.asdict() == {"var1": {"var3": "value3"}, "var2": "value2"}
+
+
+def test_update_with_flat_content():
+    # Test that an existing Environment can be
+    # updated using a flat dictionary.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    environment.update({"var2": "value2", "var3": "value3"})
+
+    assert environment.asdict() == {
+        "var1": "value1",
+        "var2": "value2",
+        "var3": "value3",
+    }
+
+
+def test_update_with_flat_content_and_namespace():
+    # Test that an existing Environment can be
+    # updated using a flat dictionary and a given namespace.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    environment.update({"var2": "value2", "var3": "value3"}, namespace="test")
+
+    assert environment.asdict() == {
+        "var1": "value1",
+        "test": {"var2": "value2", "var3": "value3"},
+    }
+
+
+def test_update_with_nested_content():
+    # Test that an existing Environment can be
+    # updated using a nested dictionary.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    environment.update({"var2": {"var3": "value3"}})
+
+    assert environment.asdict() == {"var1": "value1", "var2": {"var3": "value3"}}
+
+
+def test_update_with_nested_content_and_namespace():
+    # Test that an existing Environment can be
+    # updated using a nested dictionary and a given namespace.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    environment.update({"var2": {"var3": "value3"}}, namespace="test")
+
+    assert environment.asdict() == {
+        "var1": "value1",
+        "test": {"var2": {"var3": "value3"}},
+    }
+
+
+def test_update_deep():
+    # Check that the update operation can add
+    # keys deep into the hierarchy.
+
+    environment = Environment.from_dict({"mau": {"visitor": {"class": "someclass"}}})
+
+    environment.update(
+        {"visitor": {"custom_templates": {"template1": "value1"}}}, "mau"
+    )
+
+    assert environment.asdict() == {
+        "mau": {
+            "visitor": {
+                "class": "someclass",
+                "custom_templates": {"template1": "value1"},
+            },
+        },
+    }
+
+
+def test_set_value_flat():
+    # Check that values can be written with a
+    # dictionary-like syntax.
+
+    environment = Environment()
+
+    environment["var1"] = "value1"
+
+    assert environment.asdict() == {"var1": "value1"}
+
+
+def test_set_value_hierarchical():
+    # Check that writing a value with a dotted key
+    # results in a hierarchical dictionary.
+
+    environment = Environment()
+
+    environment["var1.var2"] = "value2"
+
+    assert environment.asdict() == {"var1": {"var2": "value2"}}
+
+
+def test_set_value_nested():
+    # Check that writing a hierarchical
+    # value results in a hierarchical dictionary.
+
+    environment = Environment()
+
+    environment["var1"] = {"var2": "value2"}
+
+    assert environment.asdict() == {"var1": {"var2": "value2"}}
+
+
+def test_set_value_empty_dict():
+    # Check that a key can be given
+    # an empty dictionary as value.
+
+    environment = Environment()
+
+    environment["var1.var2"] = {}
+
+    assert environment.asdict() == {"var1": {"var2": {}}}
+
+
+def test_get_value_flat():
+    # Check that values can be read with a
+    # dictionary-like syntax.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    assert environment["var1"] == "value1"
+
+
+def test_get_value_nested():
+    # Check that values can be read
+    # using dotted keys.
+
+    environment = Environment.from_dict({"var1": {"var2": "value2"}})
+
+    assert environment["var1.var2"] == "value2"
+
+
+def test_get_value_nested_empty():
+    # Check that empty dictionaries are
+    # supported values.
+
+    environment = Environment.from_dict({"var1": {"var2": {}}})
+
+    assert environment["var1.var2"] == {}
+
+
+def test_get_value_flat_no_key():
+    # Check that the Environment raises an
+    # exception if a flat key doesn't exist.
+
+    environment = Environment()
+
+    with pytest.raises(KeyError):
+        environment["var1"]
+
+
+def test_get_value_nested_no_key():
+    # Check that the Environment raises an
+    # exception if a nested key doesn't exist.
+
+    environment = Environment()
+
+    with pytest.raises(KeyError):
+        environment["var1.var2"]
+
+
+def test_get_value_method_flat():
+    # Check that the method get returns
+    # the correct value with a flat key.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    assert environment.get("var1") == "value1"
+
+
+def test_get_value_method_nested():
+    # Check that the method get returns
+    # the correct value with a nested key.
+
+    environment = Environment.from_dict({"var1": {"var2": "value2"}})
+
+    assert environment.get("var1.var2") == "value2"
+
+
+def test_get_value_method_accepts_default_flat():
+    # Check that the method get accepts
+    # and returns a default value.
+
+    environment = Environment.from_dict({"var1": "value1"})
+
+    assert environment.get("var2", "def") == "def"
+
+
+def test_get_value_method_accepts_default_nested():
+    # Check that the method get accepts
+    # and returns a default value.
+
+    environment = Environment.from_dict({"var1": {"var2": "value2"}})
+
+    assert environment.get("var1.var3", "def") == "def"
+
+
+def test_get_value_key_is_a_namespace_prefix():
+    environment = Environment.from_dict(
+        {
+            "top": {
+                "middle": {
+                    "var1": "value1",
+                    "var2": "value2",
+                    "var3": {},
+                }
+            },
+        }
+    )
+
+    assert environment.get("top").asdict() == {
+        "middle": {
+            "var1": "value1",
+            "var2": "value2",
+            "var3": {},
+        }
+    }
+
+    assert environment.get("top.middle").asdict() == {
+        "var1": "value1",
+        "var2": "value2",
+        "var3": {},
+    }
+
+    assert environment.get("notthere") is None
+    assert environment.get("top.mid") is None
