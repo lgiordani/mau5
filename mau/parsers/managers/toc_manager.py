9bfb9b8467bf9fa296559d27fad201edf227a411
Leonardo Giordani <giordani.leonardo@gmail.com>
2025-10-16 08:33:18 +0100

    Added TOC manager

---
+++ mau/parsers/managers/toc_manager.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import hashlib
+
+from mau.nodes.headers import HeaderNodeContent
+from mau.nodes.node import Node
+from mau.nodes.toc import TocItemNodeContent, TocNodeContent
+
+
+def default_header_unique_id(node: Node[HeaderNodeContent]) -> str:  # pragma: no cover
+    """
+    Return a unique ID for a header.
+    """
+
+    # Lowercase the text of the header.
+    text_node = node.children["text"][0]
+    text = text_node.content.value.lower()
+
+    # Find the header level
+    level = node.content.level
+
+    hashed_value = hashlib.md5(f"{level} {text}".encode("utf-8")).hexdigest()[:8]
+
+    return hashed_value
+
+
+def header_to_toc_item(header: Node[HeaderNodeContent]) -> Node[TocItemNodeContent]:
+    # Convert a the content of the node
+    # from HeadeNodeContent to TocItemNodeContent.
+
+    return Node(
+        content=TocItemNodeContent(
+            level=header.content.level,  # type: ignore[attr-defined]
+            unique_id=header.content.unique_id,  # type: ignore[attr-defined]
+        ),
+        info=header.info,
+        children={"text": header.children["text"], "entries": []},
+    )
+
+
+def add_nodes_under_level(
+    level: int,
+    nodes: list[Node[HeaderNodeContent]],
+    index: int,
+    output: list[Node[TocItemNodeContent]],
+) -> int:
+    # This recursive function transforms a list
+    # of nodes into a hierarchy.
+
+    # In this iteration, make sure we don't go
+    # past the given number of nodes.
+    while index < len(nodes):
+        # If the first node is at a higher or
+        # equal level than the current one
+        # stop the recursion.
+        if nodes[index].content.level <= level:  # type: ignore[attr-defined]
+            return index
+
+        # Add the first node.
+        node = header_to_toc_item(nodes[index])
+        output.append(node)
+
+        # Now perform the recursion on the
+        # remaining nodes, adding the
+        # children of the node that was
+        # just added.
+        index = add_nodes_under_level(
+            level=nodes[index].content.level,  # type: ignore[attr-defined]
+            nodes=nodes,
+            index=index + 1,
+            output=node.children["entries"],  # type: ignore[arg-type]
+        )
+
+    # There are no more nodes to process.
+    # if not nodes:
+    return index
+
+
+class TocManager:
+    def __init__(self, header_unique_id_function=None):
+        # This list contains the headers
+        # found parsing a document
+        self.headers: list[Node[HeaderNodeContent]] = []
+
+        # This is the list of ::toc commands
+        # that need to be updated once the ToC
+        # has been processed
+        self.toc_nodes: list[Node[TocNodeContent]] = []
+
+        self.header_unique_id_function = (
+            header_unique_id_function or default_header_unique_id
+        )
+
+        self.nested_headers: list[Node[TocItemNodeContent]] = []
+
+    def add_header(self, node: Node[HeaderNodeContent]):
+        """Add a single header to the list
+        managed headers."""
+        self.headers.append(node)
+
+    def add_toc_node(self, node: Node[TocNodeContent]):
+        """Add a single TOC node to the list
+        managed TOC nodes."""
+        self.toc_nodes.append(node)
+
+    def update(self, other: TocManager):
+        """Update the headers and toc nodes
+        with those contained in another
+        TOC Manager."""
+        self.headers.extend(other.headers)
+        self.toc_nodes.extend(other.toc_nodes)
+
+    def process(self):
+        self.nested_headers = []
+
+        for header in self.headers:
+            if header.content.unique_id is not None:
+                continue
+
+            # Create the unique ID.
+            unique_id = self.header_unique_id_function(header)
+            header.content.unique_id = unique_id
+
+        add_nodes_under_level(0, self.headers, 0, self.nested_headers)
+
+        for toc_node in self.toc_nodes:
+            toc_node.children["nested_entries"] = self.nested_headers  # type: ignore[assignment]
+            toc_node.children["plain_entries"] = self.headers  # type: ignore[assignment]
---
+++ tests/managers/test_header_links_manager.py
@@ -0,0 +1,196 @@
+# import pytest
+
+# from mau.environment.environment import Environment
+# from mau.lexers.document_lexer.lexer import DocumentLexer
+# from mau.nodes.headers import HeaderNodeContent
+# from mau.nodes.inline import TextNodeContent
+# from mau.nodes.macros import MacroHeaderNodeContent
+# from mau.nodes.node import Node, NodeInfo
+# from mau.parsers.base_parser.parser import MauParserException
+# from mau.parsers.document_parser.managers.header_links_manager import HeaderLinksManager
+# from mau.parsers.document_parser.parser import DocumentParser
+# from mau.test_helpers import (
+#     compare_nodes,
+#     generate_context,
+#     init_parser_factory,
+#     parser_runner_factory,
+# )
+
+# init_parser = init_parser_factory(DocumentLexer, DocumentParser)
+
+# runner = parser_runner_factory(DocumentLexer, DocumentParser)
+
+
+# def test_header_links_manager():
+#     ilm = HeaderLinksManager()
+
+#     header_node = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(named_args={"id": "someheader"}),
+#         children={
+#             "content": [
+#                 Node(
+#                     content=TextNodeContent("Header"),
+#                 )
+#             ]
+#         },
+#     )
+
+#     link_node = Node(
+#         content=MacroHeaderNodeContent("someheader"),
+#     )
+
+#     ilm.add_header("someheader", header_node)
+#     ilm.add_links([link_node])
+
+#     ilm.process()
+
+#     assert link_node.children["header"] == [header_node]
+
+
+# def test_header_links_manager_no_link():
+#     ilm = HeaderLinksManager()
+
+#     header_node = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(named_args={"id": "someheader"}),
+#         children={
+#             "content": [
+#                 Node(
+#                     content=TextNodeContent("Header"),
+#                 )
+#             ]
+#         },
+#     )
+
+#     ilm.add_header("someheader", header_node)
+
+#     ilm.process()
+
+
+# def test_header_links_manager_no_header():
+#     ilm = HeaderLinksManager()
+
+#     link_node = Node(
+#         content=MacroHeaderNodeContent("someheader"),
+#     )
+
+#     ilm.add_links([link_node])
+
+#     with pytest.raises(MauParserException) as exc:
+#         ilm.process()
+
+#     assert exc.value.context is link_node.info.context
+
+
+# def test_header_links_manager_duplicate_header():
+#     ilm = HeaderLinksManager()
+
+#     header_node1 = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(named_args={"id": "someheader"}),
+#     )
+
+#     header_node2 = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(named_args={"id": "someheader"}),
+#     )
+
+#     ilm.add_header("someheader", header_node1)
+
+#     with pytest.raises(MauParserException) as exc:
+#         ilm.add_header("someheader", header_node2)
+
+#     assert exc.value.context is header_node2.info.context
+
+
+# def test_header_links_manager_update():
+#     ilm_src = HeaderLinksManager()
+#     ilm_dst = HeaderLinksManager()
+
+#     header_node = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(named_args={"id": "someheader"}),
+#         children={
+#             "content": [
+#                 Node(
+#                     content=TextNodeContent("Header"),
+#                 )
+#             ]
+#         },
+#     )
+
+#     link_node = Node(
+#         content=MacroHeaderNodeContent("someheader"),
+#     )
+
+#     ilm_src.add_header("someheader", header_node)
+#     ilm_src.add_links([link_node])
+
+#     ilm_dst.update(ilm_src)
+#     ilm_dst.process()
+
+#     assert link_node.children["header"] == [header_node]
+
+
+# def test_header_links_full_parser():
+#     environment = Environment()
+#     environment.setvar(
+#         "mau.parser.header_unique_id_function",
+#         lambda node: "XXXXXX",
+#     )
+
+#     source = """
+#     This is a paragraph with an internal link [header](someid, link text).
+
+#     [id=someid]
+#     == Header
+#     """
+
+#     parser = runner(source, environment)
+
+#     header_node = Node(
+#         content=HeaderNodeContent(2, "XXXXXX"),
+#         info=NodeInfo(
+#             context=generate_context(4, 0, 4, 9), named_args={"id": "someid"}
+#         ),
+#         children={
+#             "text": [
+#                 Node(
+#                     content=TextNodeContent("Header"),
+#                     info=NodeInfo(context=generate_context(4, 3, 4, 9)),
+#                 )
+#             ],
+#         },
+#     )
+
+#     link_node = Node(
+#         content=MacroHeaderNodeContent("someid"),
+#         info=NodeInfo(context=generate_context(1, 42, 1, 69)),
+#         children={
+#             "header": [header_node],
+#             "text": [
+#                 Node(
+#                     content=TextNodeContent("link text"),
+#                     info=NodeInfo(context=generate_context(1, 59, 1, 68)),
+#                 )
+#             ],
+#         },
+#     )
+
+#     compare_nodes([parser.header_links_manager._headers["someid"]], [header_node])
+#     compare_nodes(parser.header_links_manager._links, [link_node])
+
+
+# def test_header_links_full_parser_no_header():
+#     source = """
+#     This is a paragraph with an internal link [header](someotherid, link text).
+
+#     [id=someid]
+#     == Header
+#     """
+
+#     with pytest.raises(MauParserException) as exc:
+#         runner(source)
+
+#     assert exc.value.context == generate_context(1, 42, 1, 74)
